ccbb2b08333874f2e2439e155de57487f230ed09
notme <notme@mailinator.com>
2020-01-18 17:05:43 +0100

    first commit of sample code

---
+++ src/main/java/org/example/javapath/JavaPathApplication.java
@@ -0,0 +1,13 @@
+package org.example.javapath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/javapath/JavaPathApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javapath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
