621d7faea87a7d22667526c647463a1cc807a24d
notme <notme@mailinator.com>
2020-01-18 17:18:50 +0100

    console application

--- src/main/java/org/example/javapath/JavaPathApplication.java
+++ src/main/java/org/example/javapath/JavaPathApplication.java
@@ -1,13 +1,27 @@
 package org.example.javapath;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaPathApplication {
+public class JavaPathApplication implements CommandLineRunner {
+
+	private static Logger LOG = LoggerFactory.getLogger(JavaPathApplication.class);
 
 	public static void main(String[] args) {
+		LOG.info("Starting");
+	
 		SpringApplication.run(JavaPathApplication.class, args);
+	
+		LOG.info("End");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
 	}
 
 }
